10b8da1e7f55be28e2bc8eff9712088b0d7b7c5e
kunal98v <kunal98v@gmail.com>
2024-07-12 20:53:49 +0530

    mail api

---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    protected $name;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($name)
+    {
+        Log::info("in construct");
+
+        $this->name = $name;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Welcome Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        Log::info("yes");
+        return new Content(
+            view: 'new-welcome-mail',
+            with:['name' => $this->name]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
